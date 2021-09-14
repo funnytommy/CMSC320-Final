f3b716667d84a8de6830cd681da69cbaf785e3f3
Tejan Gandhi <tejangandhi@gmail.com>
2021-09-14 11:06:45 -0400

    a generalized commit

--- RPS/play.js
+++ RPS/play.js
@@ -1,6 +1,8 @@
 (
     document.getElementById('playButton').addEventListener('click', (event) => {
-
+        event.target.classList.add('moveUp')
+        let box = document.getElementById('gamePlayer')
+        box.style.transform = "translateY(0)"
     })
 
 )();
