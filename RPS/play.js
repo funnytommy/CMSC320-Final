d480db9cd7ca54f35dec20e7cffbd4c082fdc317
Tejan Gandhi <tejangandhi@gmail.com>
2021-09-21 13:56:18 -0400

    a generalized commit

--- RPS/play.js
+++ RPS/play.js
@@ -7,3 +7,33 @@ document.getElementById('playButton').addEventListener('click', (event) => {
     setTimeout(() => { box.style.transform = "translateY(0px)" }, 3000)
 
 })
+
+window.addEventListener('resize', () => changeLayout)
+let playButton = document.getElementById("playButton")
+let gamePlayer = document.getElementById("gamePlayer")
+function changeLayout() {
+    console.log("innerheight: " + window.innerHeight)
+    console.log("innerWidth: " + window.innerWidth)
+    if (window.innerHeight > window.innerWidth) {
+        console.log("We're in portrait mode")
+        // viewing from phone or tablet.
+        playButton.classList.remove("pb_landscape")
+        playButton.classList.add("pb_portrait")
+
+        gamePlayer.classList.remove("gp_landscape")
+        gamePlayer.classList.add("gp_portrait")
+
+
+    } else {
+        console.log("We're in landscape mode")
+        // viewing from some sort of desktop.
+        playButton.classList.add("pb_landscape")
+        playButton.classList.remove("pb_portrait")
+
+        gamePlayer.classList.add("gp_landscape")
+        gamePlayer.classList.remove("gp_portrait")
+
+    }
+}
+
+changeLayout();
