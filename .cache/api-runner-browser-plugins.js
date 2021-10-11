e6bac383924fea49c42bbadf460d35e887b8d7e2
Tejan Gandhi <tejangandhi@gmail.com>
2021-10-11 19:58:57 -0400

    a generalized commit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,10 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/gatsby-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"c03d8f61a2fce1ca47c15f3b885e6537"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-smoothscroll/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
